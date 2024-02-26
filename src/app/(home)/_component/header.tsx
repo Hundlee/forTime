b62c595c1f60045336ccb0837b5c2b2d05855749
Hundlee <mesquitag884@gmail.com>
2024-02-25 21:15:19 -0300

    feat: add hello (user)

--- src/app/(home)/_component/header.tsx
+++ src/app/(home)/_component/header.tsx
@@ -3,10 +3,10 @@ import Sidebar from "./sidebar";
 /* eslint-disable @next/next/no-img-element */
 const Header = () => {
     return (
-        <div className="w-full flex items-center justify-between mt-10 px-5">
+        <header className="w-full flex items-center justify-between mt-10 px-5">
             <img src="./logoForTime.png" alt="logo" className="w-[80px]" />
             <Sidebar />
-        </div>
+        </header>
     );
 };
 
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,8 +3,14 @@ import Header from "./_component/header";
 
 export default function Home() {
     return (
-        <div>
+        <main>
             <Header />
-        </div>
+            <section className="mt-5 px-5">
+                <h1 className="text-3xl font-semibold">
+                    Hello, <span className="font-bold">Gabriel!</span>
+                </h1>
+                <h3 className="text-xl text-primary">Have a nice day!</h3>
+            </section>
+        </main>
     );
 }
