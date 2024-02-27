1bb08044505b679241877c13d0195293ead670f7
Hundlee <mesquitag884@gmail.com>
2024-02-27 15:24:23 -0300

    feat: add cardProject component

--- src/app/(home)/_component/cardProject.tsx
+++ src/app/(home)/_component/cardProject.tsx
@@ -8,8 +8,8 @@ import { User } from "lucide-react";
 
 const CardProject = () => {
     return (
-        <div className=" pl-5">
-            <Card className="w-[350px] h-[270px] rounded-[35px] flex flex-col items-start border-none justify-between shadow-xl">
+        <div className="px-5">
+            <Card className="w-full  rounded-[35px] flex flex-col items-start border-none justify-between shadow-xl">
                 <CardHeader className="flex flex-row items-center">
                     <div className="p-2 bg-indigo-900 mr-2 rounded-lg">
                         <User />
@@ -17,7 +17,7 @@ const CardProject = () => {
                     <h3>Project X</h3>
                 </CardHeader>
                 <CardContent>
-                    <h2 className="text-xl">Back End Delevopment</h2>
+                    <h2 className="text-xl py-10">Back End Delevopment</h2>
                 </CardContent>
                 <CardFooter>
                     <h3 className="text-sm">October 2020</h3>
