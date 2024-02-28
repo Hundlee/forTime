da4aac8bdeccf1973d8732c8ed8dbd5f5d006bb5
Hundlee <mesquitag884@gmail.com>
2024-02-28 16:30:27 -0300

    feat: ensure that when I click on the project I go to the project page

--- src/app/(home)/_component/cardProject.tsx
+++ src/app/(home)/_component/cardProject.tsx
@@ -1,23 +1,32 @@
+"use client";
+
 import {
     Card,
     CardContent,
     CardFooter,
     CardHeader,
 } from "@/app/_components/ui/card";
-import { db } from "@/app/_lib/prisma";
 import { User } from "lucide-react";
 import { Project } from "@prisma/client";
+import { useRouter } from "next/navigation";
 
 interface CardProjectProps {
     cardProject: Project;
 }
 
-export default async function CardProject({ cardProject }: CardProjectProps) {
-    const projects = await db.project.findMany({});
+const CardProject = ({ cardProject }: CardProjectProps) => {
+    const router = useRouter();
+
+    const handleProjectClick = () => {
+        router.push(`/project/${cardProject.id}`);
+    };
 
     return (
         <div className="px-5 my-4">
-            <Card className="w-[90vw]  rounded-[35px] flex flex-col items-start border-none justify-between shadow-lg">
+            <Card
+                className="w-[90vw]  rounded-[35px] flex flex-col items-start border-none justify-between shadow-lg"
+                onClick={handleProjectClick}
+            >
                 <CardHeader className="flex flex-row items-center">
                     <div className="p-2 bg-indigo-900 mr-2 rounded-lg">
                         <User />
@@ -25,7 +34,7 @@ export default async function CardProject({ cardProject }: CardProjectProps) {
                     <h3>{cardProject.name}</h3>
                 </CardHeader>
                 <CardContent>
-                    <h2 className="text-xl py-10">{cardProject.name}</h2>
+                    <h2 className="text-xl py-10">{cardProject.description}</h2>
                 </CardContent>
                 <CardFooter>
                     <h3 className="text-sm">October 2020</h3>
@@ -33,4 +42,6 @@ export default async function CardProject({ cardProject }: CardProjectProps) {
             </Card>
         </div>
     );
-}
+};
+
+export default CardProject;
