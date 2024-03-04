b92f069af207c2655ebe9b8723783e8f6e6a6ebb
Hundlee <mesquitag884@gmail.com>
2024-03-04 13:47:10 -0300

    feat: add page to login with email

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Label } from "../_components/ui/label";
+import { Input } from "../_components/ui/input";
+import { Button } from "../_components/ui/button";
+import { useForm } from "react-hook-form";
+
+export default function AuthForm() {
+    const form = useForm();
+
+    const handleSubmit = form.handleSubmit((data: any) => {
+        console.log(data);
+    });
+
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="flex flex-col items-center space-y-4 p-6">
+                <div className="text-center space-y-2">
+                    <h1 className="text-3xl font-bold">Login</h1>
+                    <p className="text-sm text-gray-500 dark:text-gray-400">
+                        Enter your email below to login to your account
+                    </p>
+                </div>
+                <form
+                    className="w-full max-w-sm space-y-2"
+                    onSubmit={handleSubmit}
+                >
+                    <div className="space-y-2">
+                        <Label htmlFor="email">Email</Label>
+                        <Input
+                            id="email"
+                            placeholder="m@example.com"
+                            type="email"
+                            className="border border-zinc-600 rounded-lg font-semibold focus:border-primary"
+                            {...form.register("email")}
+                        />
+                    </div>
+                    <Button className="w-full">Send Magic Link</Button>
+                    <p className="text-sm text-center text-gray-500 dark:text-gray-400">
+                        We have sent a magic link to your email address. Click
+                        the link in the email to login.
+                    </p>
+                </form>
+            </div>
+        </div>
+    );
+}
