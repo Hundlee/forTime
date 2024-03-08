a9a9205f546d73f10edc13fc77b068c98c176bd0
Hundlee <mesquitag884@gmail.com>
2024-03-07 22:36:51 -0300

    feat: update some dependencies

--- src/app/project/[id]/_actions/save-task.tsx
+++ src/app/project/[id]/_actions/save-task.tsx
@@ -5,28 +5,22 @@ import { revalidatePath } from "next/cache";
 
 interface SaveTaskParams {
     projectId: string;
-    taskId: string;
     categoryId: string;
     userId: string;
     date: Date;
     name: string;
     description: string;
-    startTime: string | Date;
-    endTime: string | Date;
 }
 
 export const saveTask = async (params: SaveTaskParams) => {
-    await db.createTask.create({
+    await db.task.create({
         data: {
             projectId: params.projectId,
-            taskId: params.taskId,
             categoryId: params.categoryId,
             userId: params.userId,
             date: params.date,
             name: params.name,
             description: params.description,
-            startTime: params.startTime,
-            endTime: params.endTime,
         },
     });
 
