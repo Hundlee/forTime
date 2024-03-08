793de74ce5ace8bbeca5bde7423bb100b8566cf2
Hundlee <mesquitag884@gmail.com>
2024-03-07 22:34:47 -0300

    feat: ensuring that only the tasks of the project to which they belong appear

--- src/app/project/_components/task.tsx
+++ src/app/project/_components/task.tsx
@@ -1,19 +1,23 @@
 import { db } from "@/app/_lib/prisma";
 import TaskItem from "./taskItem";
 
-const Task = async () => {
-    const task = await db.task.findMany({});
+const Task = async ({ projectId }: any) => {
+    const task = await db.task.findMany({
+        where: {
+            projectId: projectId,
+        },
+    });
     return (
         <main className="text-left mt-10 px-6">
             <h1 className="text-2xl font-semibold">Task</h1>
 
             <div className="flex flex-col gap-3 mt-5 h-full overflow-hidden">
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
+                {task &&
+                    task.map((item) => (
+                        <div key={item.id}>
+                            <TaskItem task={item} />
+                        </div>
+                    ))}
             </div>
         </main>
     );
