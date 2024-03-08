3733ffd97cc72317334093885b53866e9669c458
Hundlee <mesquitag884@gmail.com>
2024-03-07 22:40:50 -0300

    feat: create callbacks for me to pull into the application for user verification

--- src/app/_services/auth/index.ts
+++ src/app/_services/auth/index.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
-
+import { Adapter } from "next-auth/adapters";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/app/_lib/prisma";
 
@@ -15,11 +15,20 @@ export const {
         verifyRequest: "/auth",
         newUser: "/",
     },
-    adapter: PrismaAdapter(db),
+    adapter: PrismaAdapter(db) as Adapter,
     providers: [
         EmailProvider({
             server: process.env.EMAIL_SERVER,
             from: process.env.EMAIL_FROM,
         }),
     ],
+    callbacks: {
+        async session(params) {
+            const { session, user } = params;
+            return {
+                ...session,
+                user: { ...session.user, id: user.id },
+            };
+        },
+    },
 });
