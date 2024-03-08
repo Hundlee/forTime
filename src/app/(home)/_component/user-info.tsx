dbc0869dddc944443c6811abee8f3b1cc2ea404a
Hundlee <mesquitag884@gmail.com>
2024-03-08 15:16:23 -0300

    feat: add button addproject to home page sidemenu

--- src/app/(home)/_component/user-info.tsx
+++ src/app/(home)/_component/user-info.tsx
@@ -4,6 +4,7 @@ import { Avatar, AvatarFallback } from "@/app/_components/ui/avatar";
 import { Button } from "@/app/_components/ui/button";
 import { Session } from "next-auth";
 import { signOut } from "next-auth/react";
+import AddProject from "./add-project";
 
 interface UserInfoProps {
     user: Session["user"];
@@ -13,15 +14,24 @@ const UserInfo = ({ user }: UserInfoProps) => {
     if (!user) return;
 
     return (
-        <div>
+        <div className="flex flex-col gap-3 w-full">
             <div className="mt-10 flex items-center gap-2">
                 <Avatar>
                     <AvatarFallback>H</AvatarFallback>
                 </Avatar>
                 <span className="text-white">{user.email}</span>
             </div>
-            <div className="mt-10">
-                <Button variant="destructive" onClick={() => signOut()}>
+
+            <div className="w-full mt-5">
+                <AddProject />
+            </div>
+
+            <div className="">
+                <Button
+                    variant="destructive"
+                    onClick={() => signOut()}
+                    className="w-full"
+                >
                     Sign Out
                 </Button>
             </div>
