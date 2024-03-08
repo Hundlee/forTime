d33967b02b39a404f127ca7fe358c7382602ff2e
Hundlee <mesquitag884@gmail.com>
2024-03-07 22:39:24 -0300

    feat: update some dependencies if the user is not logged in

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,42 +1,44 @@
 import Header from "./_component/header";
 import { Button } from "../_components/ui/button";
-import Carousel from "./_component/carouselComponent";
 import Progress from "./_component/progress";
 import Navigator from "../_components/navigator";
-import { db } from "../_lib/prisma";
-import CarouselContainer from "./_component/carouselComponent";
+import CarouselProject from "./_component/carouselProject";
+import { auth } from "../_services/auth";
 
 export default async function Home() {
-    const projects = await db.project.findMany({});
+    const session = await auth();
 
     return (
         <main>
             <Header />
             <section className="mt-5 px-5">
                 <h1 className="text-3xl font-semibold">
-                    Hello, <span className="font-bold">Gabriel!</span>
+                    Hello,{" "}
+                    {session ? (
+                        <span className="font-bold">!</span>
+                    ) : (
+                        <span className="font-bold">Go to Login!</span>
+                    )}
                 </h1>
                 <h3 className="text-xl text-primary ">Have a nice day!</h3>
             </section>
             <div className="mt-10 px-5">
                 <Button className="mr-4 rounded-full bg-secondary-foreground font-bold text-black gap-4 shadow-md">
-                    My tasks{" "}
-                    <span className="border-2 border-primary rounded-full">
-                        {" "}
-                    </span>
+                    Project{" "}
+                    <span className="border-2 border-primary rounded-full"></span>
                 </Button>
                 <Button className="mr-4 rounded-full bg-secondary-foreground text-secondary font-bold gap-4">
-                    Project
+                    tasks
                 </Button>
                 <Button className="mr-4 rounded-full bg-secondary-foreground text-secondary font-bold gap-4">
                     Note
                 </Button>
             </div>
             <section className="overflow-x-auto [&::-webkit-scrollbar]:hidden flex">
-                <CarouselContainer />
+                <CarouselProject />
             </section>
 
-            <Progress />
+            {session && <Progress />}
             <Navigator />
         </main>
     );
