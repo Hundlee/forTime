b773b3391d5639b84b449bc53c953c24ecea3b00
Hundlee <mesquitag884@gmail.com>
2024-03-10 16:51:12 -0300

    feat: ensure that pending tasks appear on the home page

--- src/app/(home)/_component/progress.tsx
+++ src/app/(home)/_component/progress.tsx
@@ -1,18 +1,25 @@
-import CardProgress from "./cardProgress";
+import TaskItem from "@/app/_components/taskItem";
+import { db } from "@/app/_lib/prisma";
 
-const Progress = () => {
+export default async function Progress() {
+    const tasks = await db.task.findMany({});
+
     return (
-        <>
-            <div className="px-5 mt-5 w-full">
-                <h1 className="text-xl font-semibold">Progress</h1>
-            </div>
-            <div className="overflow-x-auto [&::-webkit-scrollbar]:hidden flex gap-5 px-5 mt-2">
-                <CardProgress />
-                <CardProgress />
-                <CardProgress />
-            </div>
-        </>
+        <div>
+            {tasks.length > 0 && (
+                <>
+                    <div className="px-5 mt-2 w-full">
+                        <h1 className="text-xl font-semibold">Progress</h1>
+                    </div>
+                    <div className="overflow-x-auto [&::-webkit-scrollbar]:hidden flex gap-5 px-5 ">
+                        {tasks.map((task) => (
+                            <div key={task.id} className="my-3">
+                                <TaskItem task={task} />
+                            </div>
+                        ))}
+                    </div>
+                </>
+            )}
+        </div>
     );
-};
-
-export default Progress;
+}
